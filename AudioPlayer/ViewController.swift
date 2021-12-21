6a82289687bad47416163f4f2a8c3d602ab935b0
duxxless <duxxless53@ya.ru>
2021-12-21 21:29:09 +0300

    Initial Commit

---
+++ AudioPlayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AudioPlayer
+//
+//  Created by Duxxless on 21.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
